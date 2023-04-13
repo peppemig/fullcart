d3487c62b33174ac32c42d11bbc5e5f000b2ae85
Giuseppe <120139042+peppemig@users.noreply.github.com>
2023-04-13 18:39:24 +0200

    added google auth + basic dashboard layout

---
+++ admin-dashboard/components/Nav.jsx
@@ -0,0 +1,47 @@
+import Link from "next/link"
+import {GiShoppingBag} from "react-icons/gi"
+import {AiOutlineHome} from "react-icons/ai"
+import {BiCog} from "react-icons/bi"
+import {FaClipboardList} from "react-icons/fa"
+import {BsFillBoxFill} from "react-icons/bs"
+import { useRouter } from "next/router"
+
+export default function Nav() {
+    const inactiveLink = "flex gap-1 items-center font-semibold text-md p-1 hover:bg-gray-200 hover:text-blue-900 transition rounded-lg"
+    const activeLink = inactiveLink + " bg-white text-blue-900 rounded-lg"
+    const router = useRouter()
+    const {pathname} = router
+
+    return (
+        <aside className="text-white p-4">
+
+            <Link href={'/'} className="flex flex-col items-center justify-center mb-5">
+                <div className="flex items-center font-bold text-xl">
+                    <GiShoppingBag size={35}/>
+                    <span>Fullcart</span>   
+                </div>
+                <div className="font-semibold text-md">Admin Panel</div>
+            </Link>
+
+            <nav className="flex flex-col gap-2">
+                <Link className={pathname === '/' ? activeLink : inactiveLink} href={'/'}>
+                    <AiOutlineHome size={25}/>
+                    Dashboard
+                </Link>
+                <Link className={pathname.includes('/products') ? activeLink : inactiveLink} href={'/products'}>
+                    <BsFillBoxFill size={25}/>
+                    Products
+                </Link>
+                <Link className={pathname.includes('/orders') ? activeLink : inactiveLink} href={'/orders'}>
+                    <FaClipboardList size={25}/>
+                    Orders
+                </Link>
+                <Link className={pathname.includes('/settings') ? activeLink : inactiveLink} href={'/settings'}>
+                    <BiCog size={25}/>
+                    Settings
+                </Link>
+            </nav>
+
+        </aside>
+    )
+}
---
+++ admin-dashboard/pages/index.js
@@ -0,0 +1,22 @@
+import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+
+export default function Home() {
+  const {data: session} = useSession()
+
+  return (
+    <Layout>
+      <div className="text-blue-900 flex justify-between">
+        <div className="flex items-center gap-2">
+          <h1 className="font-semibold">Hello, {session?.user?.name}</h1>
+        </div>
+
+        <div className="flex gap-1 py-1 px-2 rounded-md items-center text-black bg-gray-300">
+          <Image src={session?.user?.image} alt="user-image" width={30} height={30} className="rounded-full"/>
+          <h1 className="font-semibold">{session?.user?.name}</h1>
+        </div>
+      </div>
+    </Layout>
+  )
+}
